384cd19f182f479641890650509c9347f54b9828
valdomirocm2016 <valdomirocm@gmail.com>
2019-07-01 14:28:10 -0300

    Criando a consulta por nome no clientes.js

--- routes/clientes.js
+++ routes/clientes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const Cliente = require('../models/clientes');
 const router = express.Router();
+const Sequelize = require('sequelize');
+const Op= Sequelize.Op;
 
 router.get("/", (req,res) =>
     Cliente.findAll()
@@ -8,9 +10,10 @@ router.get("/", (req,res) =>
        .catch(error => {
             res.status(412).json({msg: error.message});
        }));
-       router.get("/:id",(req,res) => {
-          Cliente.findOne({
-               where:{
+
+router.get("/:id",(req,res) => {
+     Cliente.findOne({
+          where:{
                     codigo: req.params.id,
                }
           }).then(result => {
@@ -23,7 +26,15 @@ router.get("/", (req,res) =>
               res.status(412).json({msg: erro.message});
           });
          })
- 
+router.get('/buscar/var', (req, res) => {
+     var query = `%${req.query.nome}%`;
+     console.log(query)
+          Cliente.findAll({ where:{ nome: { [Op.like]: query } } })
+               .then(clientes => res.json(clientes))     
+               .catch( err => console.log(err));
+                   
+});
+              
  
  
 

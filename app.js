af0840ff5ada3e07b67928661207ae21cd431097
valdomirocm2016 <valdomirocm@gmail.com>
2019-07-01 13:56:24 -0300

    Criando o projeto

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+const db = require('./config/database');
+
+db.authenticate()
+.then(() => console.log('Database connected...'))
+.catch(err => console.log('Error'+err))
+
+
+
+app.set("json spaces",4);
+
+const index= require('./routes/index');
+const clientes = require('./routes/clientes');
+
+app.use('/',index);
+app.use('/clientes', clientes);
+
+app.get("/", (req,res)=> res.json({status: "Nodejs backend"}));
+
+app.listen(PORT, ()=> console.log("escutando na porta "+PORT));
